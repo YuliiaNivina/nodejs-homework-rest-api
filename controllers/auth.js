524e142b21c156e2898f2860f74a88a4133ff9ea
Yuliia Nivina <y.nivina@gmail.com>
2023-08-05 01:04:50 +0300

    Add login

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,10 +1,18 @@
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const { User, joiUserSchema } = require("../models/user");
 const ResultError = require("../utils/ResultError");
 const ctrlWrapper = require("../utils/ctrlWrapper");
 
+const { SECRET_KEY } = process.env;
+
 const register = async (req, res) => {
+  const { error } = joiUserSchema.validate(req.body);
+  if (error) {
+    throw ResultError(400, "missing required name field");
+  }
+
   const { email, password } = req.body;
   const user = await User.findOne({ email });
   if (user) {
@@ -23,6 +31,41 @@ const register = async (req, res) => {
   });
 };
 
+const login = async (req, res) => {
+  const { error } = joiUserSchema.validate(req.body);
+  if (error) {
+    throw ResultError(400, "missing required name field");
+  }
+
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw ResultError(401, "Email or password is wrong");
+  }
+
+  const passwordCompare = await bcrypt.compare(password, user.password);
+
+  if (!passwordCompare) {
+    throw ResultError(401, "Email or password is wrong");
+  }
+
+  const payload = {
+    id: user._id,
+  }
+
+  const token= jwt.sign(payload, SECRET_KEY, {expiresIn: "23h"});
+
+  res.status(201).json({
+    token,
+    user: {
+      email: user.email,
+      subscription: user.subscription,
+    },
+  });
+};
+
 module.exports = {
   register: ctrlWrapper(register),
+  login: ctrlWrapper(login),
 };
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -5,4 +5,6 @@ const ctrl = require("../../controllers/auth")
 
 router.post("/register", ctrl.register);
 
+router.post("/login", ctrl.login);
+
 module.exports = router;
