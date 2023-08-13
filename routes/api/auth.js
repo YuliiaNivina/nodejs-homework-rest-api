41f30f18499d5459ff0a5967ba9d3fcf0754ae2d
Yuliia Nivina <y.nivina@gmail.com>
2023-08-14 01:31:28 +0300

    Add send email

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -7,12 +7,21 @@ const upload = require("../../middlewares/upload");
 
 router.post("/register", ctrl.register);
 
+router.get("/verify/:verificationToken", ctrl.verifyEmail);
+
+router.post("/verify", ctrl.resendVerifyEmail);
+
 router.post("/login", ctrl.login);
 
 router.get("/current", authenticate, ctrl.getCurrent);
 
 router.post("/logout", authenticate, ctrl.logout);
 
-router.patch("/avatars", authenticate, upload.single("avatar"), ctrl.updateAvatar)
+router.patch(
+  "/avatars",
+  authenticate,
+  upload.single("avatar"),
+  ctrl.updateAvatar
+);
 
 module.exports = router;
